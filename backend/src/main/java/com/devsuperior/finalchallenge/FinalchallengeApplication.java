fa2284206c5e391fe9f370f2cb49dc12b5af0f62
Undersin <andersonvieira818@yahoo.com.br>
2022-11-10 08:24:56 -0300

    Starting challange

---
+++ backend/src/main/java/com/devsuperior/finalchallenge/FinalchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.finalchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalchallengeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/finalchallenge/FinalchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.finalchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
